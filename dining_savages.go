3f31690444145b6573ca51040d7d67a82fb01779
fattouche <alex.fattouche@gmail.com>
2018-09-18 17:19:59 -0700

    Add some comments

--- dining_savages.go
+++ dining_savages.go
@@ -17,11 +17,14 @@ func main() {
 		pot <- 1
 	}
 	potFull <- 1
+	//Start all savages concurrently
 	for i := 0; i < savages; i++ {
 		wg.Add(1)
 		go eat(potEmpty, pot, potFull)
 	}
+	//Start the cook
 	cook(potEmpty, pot, potFull)
+	//Make sure we wait for all savages to eat
 	wg.Wait()
 }
 
@@ -34,6 +37,7 @@ func cook(potEmpty, pot, potFull chan int) {
 		for i := 0; i < servingSize; i++ {
 			pot <- 1
 		}
+		//Notify pot is full
 		potFull <- 1
 	}
 }
@@ -41,13 +45,16 @@ func cook(potEmpty, pot, potFull chan int) {
 func eat(potEmpty, pot, potFull chan int) {
 	defer wg.Done()
 	for {
+		//Wait till the pot is full, ensures we don't eat untill cook fills it fully
 		<-potFull
 		select {
 		case <-pot:
+			//Eat from pot
 			fmt.Printf("eating\n")
 			potFull <- 1
 			return
 		default:
+			//Pot is empty, need to notify cook
 			potEmpty <- 1
 		}
 	}
